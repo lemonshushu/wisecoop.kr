41c911405454a6af473599e2af3682f161fd171f
lemonshushu <ifnt7luv@gmail.com>
2021-03-14 12:49:50 +0900

    Updated Carousel

--- src/features/intro/IntroCarousel.js
+++ src/features/intro/IntroCarousel.js
@@ -24,7 +24,7 @@ function IntroCarousel() {
   };
 
   return (
-    <Carousel style={{maxWidth: 700, margin: 'auto'}} activeIndex={index} onSelect={handleSelect} nextIcon={nextIcon} prevIcon={prevIcon} pause='hover'>
+    <Carousel style={{maxWidth: 800, margin: 'auto'}} activeIndex={index} onSelect={handleSelect} nextIcon={nextIcon} prevIcon={prevIcon} pause='hover'>
       <Carousel.Item>
         <img
           className="d-block w-100"
